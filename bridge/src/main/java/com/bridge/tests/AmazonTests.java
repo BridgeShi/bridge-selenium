51d5aad7ef14d6c6a07d39faf95c130fb08ce3a2
Bridge Shi <bridge@extrabux.com>
2016-04-20 15:12:18 +0800

    BaseTest

--- bridge/src/main/java/com/bridge/tests/AmazonTests.java
+++ bridge/src/main/java/com/bridge/tests/AmazonTests.java
@@ -3,27 +3,16 @@ package com.bridge.tests;
 
 import java.util.List;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeTest;
 import com.bridge.dao.AmazonDAO;
 import com.bridge.pages.amazon.CartPage;
 import com.bridge.pages.amazon.ProductPage;
 
-
-public class AmazonTests {
-
-	private static WebDriver driver;
+public class AmazonTests extends BaseTest{
 	
 	private static AmazonDAO amazonDAO = new AmazonDAO();
 	
-	@BeforeTest
-	public void getWebDriver(){
-		AmazonTests.driver = new FirefoxDriver();
-	}
-	
 	public static void main(String args[]){
-		driver = new FirefoxDriver();
+		getWebDriver();
 		
 		List<String> productUrlList = amazonDAO.getUrlList(2);
 		
@@ -31,10 +20,7 @@ public class AmazonTests {
 			getInventory(productUrl);
 		}
 		
-		//关闭webdriver
-		driver.close();
-		
-		driver.quit();
+		closeWebDriver();
 	}
 	
 	//@Test
